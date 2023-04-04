55b443cc4feb7c5ab2f8d8658effb6c75746f342
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-04-04 17:59:20 +0100

    Add alignment info to the simple tests

--- tests/test_placeholders.py
+++ tests/test_placeholders.py
@@ -8,21 +8,24 @@ from opustrainer.modifiers.placeholders import PlaceholderTagModifier
 class TestTagger(unittest.TestCase):
 	def test_tagger_tagging(self):
 		random.seed(1)
-		tagger = PlaceholderTagModifier(probability=1)
-		output = tagger('Hello world\tHallo Welt\t0-0 1-1')
-		self.assertEqual(output, 'Hello <tag4> Hallo </tag4> world <tag1> Welt </tag1>\tHallo Welt')
+		tagger = PlaceholderTagModifier(probability=.5)
+		output = tagger('Hello world etcetera\tHallo Welt und so\t0-0 1-1 2-2 2-3')
+		self.assertEqual(output, 'Hello <tag4> Hallo </tag4> world <tag1> Welt </tag1> etcetera\tHallo Welt und so\t0-0 4-1 8-2 8-3')
+		#                         ^0    ^(1)   ^(2)  ^(3)    ^4    ^(5)   ^(6) ^(7)    ^8        ^0    ^1   ^2  ^3
 
 	def test_tagger_augment(self):
 		random.seed(1)
-		tagger = PlaceholderTagModifier(probability=1, augment=1)
-		output = tagger('Hello world\tHallo Welt\t0-0 1-1')
-		self.assertEqual(output, 'Hello ॸऍॳनःं ः॓ॅँॸ॰ रॗछॼशड़ world ټ؇ۤە٣ٮڛۃ\tHallo ॸऍॳनःं ः॓ॅँॸ॰ रॗछॼशड़ Welt ټ؇ۤە٣ٮڛۃ')
+		tagger = PlaceholderTagModifier(probability=.5, augment=1)
+		output = tagger('Hello world etcetera\tHallo Welt und so\t0-0 1-1 2-2 2-3')
+		self.assertEqual(output, 'Hello world ټ؇ۤە٣ٮڛۃ etcetera\tHallo Welt ټ؇ۤە٣ٮڛۃ und so\t0-0 1-1 2-2 3-3 3-4')
+		#                         ^0    ^1    ^2       ^3        ^0    ^1   ^2       ^3  ^4
 
 	def test_tagger_replace(self):
 		random.seed(1)
 		tagger = PlaceholderTagModifier(probability=.5, replace=1)
-		output = tagger('Hello world\tHallo Welt\t0-0 1-1')
-		self.assertEqual(output, 'Hello world <tag1> ټ؇ۤە٣ٮڛۃ </tag1>\tHallo ټ؇ۤە٣ٮڛۃ')
+		output = tagger('Hello world etcetera\tHallo Welt und so\t0-0 1-1 2-2 2-3')
+		self.assertEqual(output, 'Hello world <tag1> ټ؇ۤە٣ٮڛۃ </tag1> etcetera\tHallo ټ؇ۤە٣ٮڛۃ und so\t0-0 1-1 5-2 5-3')
+		#                         ^0    ^1    ^(2)   ^(3)     ^(4)    ^5        ^0    ^1       ^2  ^3
 
 	def test_tagger_zh_src(self):
 		'''Tests the tagger with zh on the source side'''
