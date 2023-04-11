d8bc028478700e8e220a30cf584ec998e542d21e
Nikolay Bogoychev <nheart@gmail.com>
2023-04-11 22:56:01 +0100

    Noise augmenters (#14)
    * wip
    
    * Initial implementation of noise augmenters
    
    * Simplify code a bit
    
    * Fix tests
    
    * Fix possible bug in test
    
    * Add specific tests for the three modes
    
    * Reduce the augmentation length
    
    ---------
    
    Co-authored-by: Jelmer van der Linde <jelmer@ikhoefgeen.nl>

--- src/opustrainer/modifiers/placeholders.py
+++ src/opustrainer/modifiers/placeholders.py
@@ -1,11 +1,170 @@
 import random
 from operator import itemgetter
-from typing import Set, List, Tuple, Optional, Protocol
+from typing import Set, List, Tuple, Optional, Protocol, TypeVar, Iterable
 from warnings import warn
 
 from sacremoses import MosesDetokenizer
 
 from opustrainer.modifiers import Modifier
+
+
+T = TypeVar('T')
+
+def random_weighted_choice(options:Iterable[Tuple[T,float]]) -> T:
+    choice = random.random()
+    cumsum = 0
+    for option, prob in options:
+        cumsum += prob
+        if choice < cumsum:
+            return option
+    raise RuntimeError('random_weighted_choice called with cumulative sum smaller than 1.0')
+
+
+
+def get_random_unicode_string(min_length: int=1, max_length: int=4, max_words: int=3) -> str:
+    """Gets a random unicode string of words, of up to max_words, where each word is of length
+    min_length-max_length. Only one character set per invocation.
+    Maybe should do special rules for emoji and CJK? Emoji wouldn't appear with spaces in between normally
+    and CJK would normally be of maximum length of 3 per word."""
+
+    length: int = random.randint(min_length, max_length)
+
+    # Update this to include code point ranges to be sampled
+    # https://jrgraphix.net/r/Unicode/
+    # https://en.wikipedia.org/wiki/Unicode_block
+    include_ranges = [
+        (0x0020, 0x007F), # Basic Latin
+        (0x00A0, 0x00FF), # Latin-1 Supplement
+       # (0x0100, 0x017F), # Latin Extended-A
+       # (0x0180, 0x024F), # Latin Extended-B
+       # (0x0250, 0x02AF), # IPA Extensions
+       # (0x02B0, 0x02FF), # Spacing Modifier Letters
+       # (0x0300, 0x036F), # Combining Diacritical Marks
+        (0x0370, 0x03FF), # Greek and Coptic
+        (0x0400, 0x04FF), # Cyrillic
+       # (0x0500, 0x052F), # Cyrillic Supplementary
+        (0x0530, 0x058F), # Armenian
+        (0x0590, 0x05FF), # Hebrew
+        (0x0600, 0x06FF), # Arabic
+       # (0x0700, 0x074F), # Syriac
+       # (0x0780, 0x07BF), # Thaana
+        (0x0900, 0x097F), # Devanagari
+        (0x0980, 0x09FF), # Bengali
+       # (0x0A00, 0x0A7F), # Gurmukhi
+        (0x0A80, 0x0AFF), # Gujarati
+       # (0x0B00, 0x0B7F), # Oriya
+       # (0x0B80, 0x0BFF), # Tamil
+       # (0x0C00, 0x0C7F), # Telugu
+       # (0x0C80, 0x0CFF), # Kannada
+       # (0x0D00, 0x0D7F), # Malayalam
+       # (0x0D80, 0x0DFF), # Sinhala
+        (0x0E00, 0x0E7F), # Thai
+       # (0x0E80, 0x0EFF), # Lao
+       # (0x0F00, 0x0FFF), # Tibetan
+        (0x1000, 0x109F), # Myanmar
+        (0x10A0, 0x10FF), # Georgian
+       # (0x1100, 0x11FF), # Hangul Jamo
+       # (0x1200, 0x137F), # Ethiopic
+       # (0x13A0, 0x13FF), # Cherokee
+       # (0x1400, 0x167F), # Unified Canadian Aboriginal Syllabics
+       # (0x1680, 0x169F), # Ogham
+       # (0x16A0, 0x16FF), # Runic
+       # (0x1700, 0x171F), # Tagalog
+       # (0x1720, 0x173F), # Hanunoo
+       # (0x1740, 0x175F), # Buhid
+       # (0x1760, 0x177F), # Tagbanwa
+        (0x1780, 0x17FF), # Khmer
+       # (0x1800, 0x18AF), # Mongolian
+       # (0x1900, 0x194F), # Limbu
+       # (0x1950, 0x197F), # Tai Le
+       # (0x19E0, 0x19FF), # Khmer Symbols
+       # (0x1D00, 0x1D7F), # Phonetic Extensions
+       # (0x1E00, 0x1EFF), # Latin Extended Additional
+       # (0x1F00, 0x1FFF), # Greek Extended
+       # (0x2000, 0x206F), # General Punctuation
+       # (0x2070, 0x209F), # Superscripts and Subscripts
+       # (0x20A0, 0x20CF), # Currency Symbols
+       # (0x20D0, 0x20FF), # Combining Diacritical Marks for Symbols
+       # (0x2100, 0x214F), # Letterlike Symbols
+       # (0x2150, 0x218F), # Number Forms
+       # (0x2190, 0x21FF), # Arrows
+       # (0x2200, 0x22FF), # Mathematical Operators
+       # (0x2300, 0x23FF), # Miscellaneous Technical
+       # (0x2400, 0x243F), # Control Pictures
+       # (0x2440, 0x245F), # Optical Character Recognition
+       # (0x2460, 0x24FF), # Enclosed Alphanumerics
+       # (0x2500, 0x257F), # Box Drawing
+       # (0x2580, 0x259F), # Block Elements
+       # (0x25A0, 0x25FF), # Geometric Shapes
+       # (0x2600, 0x26FF), # Miscellaneous Symbols
+       # (0x2700, 0x27BF), # Dingbats
+       # (0x27C0, 0x27EF), # Miscellaneous Mathematical Symbols-A
+       # (0x27F0, 0x27FF), # Supplemental Arrows-A
+       # (0x2800, 0x28FF), # Braille Patterns
+       # (0x2900, 0x297F), # Supplemental Arrows-B
+       # (0x2980, 0x29FF), # Miscellaneous Mathematical Symbols-B
+       # (0x2A00, 0x2AFF), # Supplemental Mathematical Operators
+       # (0x2B00, 0x2BFF), # Miscellaneous Symbols and Arrows
+       # (0x2E80, 0x2EFF), # CJK Radicals Supplement
+       # (0x2F00, 0x2FDF), # Kangxi Radicals
+       # (0x2FF0, 0x2FFF), # Ideographic Description Characters
+       # (0x3000, 0x303F), # CJK Symbols and Punctuation
+        (0x3040, 0x309F), # Hiragana
+        (0x30A0, 0x30FF), # Katakana
+       # (0x3100, 0x312F), # Bopomofo
+       # (0x3130, 0x318F), # Hangul Compatibility Jamo
+       # (0x3190, 0x319F), # Kanbun
+       # (0x31A0, 0x31BF), # Bopomofo Extended
+       # (0x31F0, 0x31FF), # Katakana Phonetic Extensions
+       # (0x3200, 0x32FF), # Enclosed CJK Letters and Months
+       # (0x3300, 0x33FF), # CJK Compatibility
+       # (0x3400, 0x4DBF), # CJK Unified Ideographs Extension A
+       # (0x4DC0, 0x4DFF), # Yijing Hexagram Symbols
+        (0x4E00, 0x9FFF), # CJK Unified Ideographs
+       # (0xA000, 0xA48F), # Yi Syllables
+       # (0xA490, 0xA4CF), # Yi Radicals
+        (0xAC00, 0xD7AF), # Hangul Syllables
+       # (0xD800, 0xDB7F), # High Surrogates
+       # (0xDB80, 0xDBFF), # High Private Use Surrogates
+       # (0xDC00, 0xDFFF), # Low Surrogates
+       # (0xE000, 0xF8FF), # Private Use Area
+       # (0xF900, 0xFAFF), # CJK Compatibility Ideographs
+       # (0xFB00, 0xFB4F), # Alphabetic Presentation Forms
+       # (0xFB50, 0xFDFF), # Arabic Presentation Forms-A
+       # (0xFE00, 0xFE0F), # Variation Selectors
+       # (0xFE20, 0xFE2F), # Combining Half Marks
+       # (0xFE30, 0xFE4F), # CJK Compatibility Forms
+       # (0xFE50, 0xFE6F), # Small Form Variants
+       # (0xFE70, 0xFEFF), # Arabic Presentation Forms-B
+       # (0xFF00, 0xFFEF), # Halfwidth and Fullwidth Forms
+       # (0xFFF0, 0xFFFF), # Specials
+       # (0x10000, 0x1007F), # Linear B Syllabary
+       # (0x10080, 0x100FF), # Linear B Ideograms
+       # (0x10100, 0x1013F), # Aegean Numbers
+       # (0x10300, 0x1032F), # Old Italic
+       # (0x10330, 0x1034F), # Gothic
+       # (0x10380, 0x1039F), # Ugaritic
+       # (0x10400, 0x1044F), # Deseret
+       # (0x10450, 0x1047F), # Shavian
+       # (0x10480, 0x104AF), # Osmanya
+       # (0x10800, 0x1083F), # Cypriot Syllabary
+       # (0x1D000, 0x1D0FF), # Byzantine Musical Symbols
+       # (0x1D100, 0x1D1FF), # Musical Symbols
+       # (0x1D300, 0x1D35F), # Tai Xuan Jing Symbols
+       # (0x1D400, 0x1D7FF), # Mathematical Alphanumeric Symbols
+        (0x1F600, 0x1F64F),  # Emoji
+       # (0x20000, 0x2A6DF), # CJK Unified Ideographs Extension B
+       # (0x2F800, 0x2FA1F), # CJK Compatibility Ideographs Supplement
+       # (0xE0000, 0xE007F), # Tags
+    ]
+    # Select a character set
+    alphabet = random.choice(include_ranges)
+    
+    # Generate a random string of 1 - 3 words
+    return ' '.join(
+        ''.join(chr(random.randrange(*alphabet)) for _ in range(length))
+        for _ in range(random.randint(1, max_words))
+    )
 
 
 def tuplify(pair: str) -> Tuple[int, int]:
@@ -83,17 +242,20 @@ class PlaceholderTagModifier(Modifier):
          custom_detok_src: 'zh'
          custom_detok_trg: null
          template: " <tag{n}> {token} </tag{n}>"
+         augment: 0.0 # 0% chance to just insert a random string on both sides
+         replace: 0.0 # 0% change to use tags to force translate to a random string
         ```
     """
 
     num_tags: int
     template: str
     src_detokenizer: Detokenizer
-    trg_detokenizer: Optional[Detokenizer]
+    trg_detokenizer: Detokenizer
+    modes: List[Tuple[str,float]]
 
     def __init__(self, probability: float=0.0, num_tags: int=6,
         custom_detok_src: Optional[str]=None, custom_detok_trg: Optional[str]=None,
-        template: str=" <tag{n}> {token} </tag{n}>"):
+        template: str=" <tag{n}> {token} </tag{n}>", augment: float=0, replace:float=0):
         super().__init__(probability)
 
         self.num_tags = num_tags
@@ -107,8 +269,20 @@ class PlaceholderTagModifier(Modifier):
         if custom_detok_trg:
             self.trg_detokenizer = MosesDetokenizer(lang=custom_detok_trg)
         else:
-            self.trg_detokenizer = None
+            self.trg_detokenizer = SpaceDetokenizer()
 
+        self.modes = []
+
+        if augment + replace > 1.0:
+            raise ValueError('sum of augment and replace probability should not exceed 1.0')
+
+        if augment > 0:
+            self.modes.append(('augment', augment))
+
+        if replace > 0:
+            self.modes.append(('replace', replace))
+
+        self.modes.append(('tag', 1.0)) # Weight doesn't matter as long as cumsum => 1.0, it's last on the list anyway
 
     def __call__(self, line:str) -> str:
         """Applies tag to words in a line based on alignment info, and then removes the alignment info from the line.
@@ -116,6 +290,7 @@ class PlaceholderTagModifier(Modifier):
            eg "I like cake" would become "I like <tag0> gusta </tag0> cake. By default the detokenizer used is the trivial
            detokenizer, but we can instead have separate detokenizers on src and trg."
         """
+
         src, trg, alignment = line.strip().split('\t')
         source = src.split(' ')
         target = trg.split(' ')
@@ -135,24 +310,40 @@ class PlaceholderTagModifier(Modifier):
             # Skip words that are the same on 
             if source[candidates[i][0]] == target[candidates[i][1]]:
                 continue
-            
+
             # Skip words whose turn it isn't yet.
             if random.random() >= self.probability:
                 continue
+            
+            # Select mode (skip random_weighted_choices*() when 'tag' is the only mode)
+            mode = random_weighted_choice(self.modes) if len(self.modes) > 1 else 'tag'
 
-            # We run out of tags so no point of trying to tag anything else
-            if not tags:
-                break
+            # If we'd need a tag but we ran out of them, skip.
+            if (mode == "tag" or mode == "replace") and not tags:
+                continue
 
-            tag_id = tags.pop()
-            source[candidates[i][0]] = source[candidates[i][0]] + self.template.format(n=tag_id, token=target[candidates[i][1]])
+            if mode == "tag":
+                # Hint the expected output to the trainer by specifying it in the input as a tag
+                source[candidates[i][0]] += self.template.format(n=tags.pop(), token=target[candidates[i][1]])
+            elif mode == "replace":
+                # Same as above, but instead of the expected target word, we replace it on both
+                # sides with a random string. This encourages the model to really ignore the source
+                # and produce the target that we desire.
+                augment = get_random_unicode_string()
+                source[candidates[i][0]] += self.template.format(n=tags.pop(), token=augment)
+                target[candidates[i][1]] = augment
+            elif mode == "augment":
+                # Augment mode adds random noise both on the source and the target without any
+                # tagging encouraging the model to copy crap from one side to the other.
+                augment = get_random_unicode_string()
+                source[candidates[i][0]] = source[candidates[i][0]] + " " + augment
+                target[candidates[i][1]] = target[candidates[i][1]] + " " + augment
 
-        source_detok: str = self.src_detokenizer.detokenize(source)
-        if self.trg_detokenizer is not None:
-            trg = self.trg_detokenizer.detokenize(target)
+        source_detok = self.src_detokenizer.detokenize(source)
+        target_detok = self.trg_detokenizer.detokenize(target)
 
         # Return the sentence, source tagged a la Dinu et al, target as it is and no alignment info
-        return  source_detok + "\t" + trg
+        return  source_detok + "\t" + target_detok
 
     def validate(self, context:List[Modifier]) -> None:
         """Current limitation of the tags modifier is that any other modifier might modify the
--- tests/test_placeholders.py
+++ tests/test_placeholders.py
@@ -8,6 +8,24 @@ from opustrainer.trainer import CurriculumLoader
 
 
 class TestTagger(unittest.TestCase):
+  def test_tagger_tagging(self):
+    random.seed(1)
+    tagger = PlaceholderTagModifier(probability=1)
+    output = tagger('Hello world\tHallo Welt\t0-0 1-1')
+    self.assertEqual(output, 'Hello <tag4> Hallo </tag4> world <tag1> Welt </tag1>\tHallo Welt')
+
+  def test_tagger_augment(self):
+    random.seed(1)
+    tagger = PlaceholderTagModifier(probability=1, augment=1)
+    output = tagger('Hello world\tHallo Welt\t0-0 1-1')
+    self.assertEqual(output, '''Hello ゜ そ world ټ؇ۤە	Hallo ゜ そ Welt ټ؇ۤە''')
+
+  def test_tagger_replace(self):
+    random.seed(1)
+    tagger = PlaceholderTagModifier(probability=.5, replace=1)
+    output = tagger('Hello world\tHallo Welt\t0-0 1-1')
+    self.assertEqual(output, '''Hello <tag4> ゜ そ </tag4> world <tag1> ټ؇ۤە </tag1>	゜ そ ټ؇ۤە''')
+
   def test_tagger_zh_src(self):
     '''Tests the tagger with zh on the source side'''
     random.seed(1)
@@ -36,6 +54,18 @@ class TestTagger(unittest.TestCase):
     tagger = PlaceholderTagModifier(probability=0.6, num_tags=4)
     with open('contrib/test-data/clean.enzh.10', 'r', encoding='utf-8') as myinput, \
          open('contrib/test-data/clean.enzh.ref.06.4.none', 'r', encoding='utf-8') as reference:
+        for line in myinput:
+          test = tagger(line)
+          ref = reference.readline()[:-1]
+          self.assertEqual(test, ref)
+
+  def test_tagger_zh_src_augment_replace(self):
+    '''Tests the tagger with zh on the source side'''
+    random.seed(1)
+    tagger = PlaceholderTagModifier(probability=0.6, num_tags=4, custom_detok_src='zh', custom_detok_trg=None,
+          template=" <tag{n}> {token} </tag{n}>", augment=0.4, replace=0.4)
+    with open('contrib/test-data/clean.zhen.10', 'r', encoding='utf-8') as myinput, \
+         open('contrib/test-data/clean.zhen.ref.06.4.04.04.src', 'r', encoding='utf-8') as reference:
         for line in myinput:
           test = tagger(line)
           ref = reference.readline()[:-1]
