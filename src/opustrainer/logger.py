b9c854590d97fa1f809eb0b285687b382e0a9a5c
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-07-04 12:53:36 +0100

    Backport to 3.8

--- src/opustrainer/logger.py
+++ src/opustrainer/logger.py
@@ -1,7 +1,7 @@
 from io import TextIOWrapper
 import logging
-from sys import stderr
-from typing import List, Dict, TextIO, Union
+from sys import stderr, version_info
+from typing import List, Dict, TextIO, Union, Optional
 from functools import lru_cache
 
 def _getLevelNamesMapping() -> Dict[str,int]:
@@ -25,7 +25,7 @@ def get_log_level(name: str) -> int:
     if name.upper() in _getLevelNamesMapping():
         return _getLevelNamesMapping()[name.upper()]
     else:
-        logging.log(logging.WARNING, "unknown log level level used: " + name + " assuming warning...")
+        logging.log(logging.WARNING, f"unknown log level level used: {name} assuming warning...")
         return logging.WARNING
 
 def log(msg: str, loglevel: str = "INFO") -> None:
@@ -39,7 +39,7 @@ def log_once(msg: str, loglevel: str = "INFO") -> None:
     log(msg, loglevel)
 
 
-def setup_logger(outputfilename: Union[str, None] = None, loglevel: str = "INFO", disable_stderr: bool=False) -> None:
+def setup_logger(outputfilename: Optional[str] = None, loglevel: str = "INFO", disable_stderr: bool=False) -> None:
     """Sets up the logger with the necessary settings. Outputs to both file and stderr"""
     loggingformat = '[%(asctime)s] [Trainer] [%(levelname)s] %(message)s'
     handlers: List[Union[logging.StreamHandler[TextIO], logging.StreamHandler[TextIOWrapper]]] = []
@@ -52,8 +52,14 @@ def setup_logger(outputfilename: Union[str, None] = None, loglevel: str = "INFO"
         handlers.append(logging.StreamHandler(stream=stderr))
     if outputfilename is not None:
         handlers.append(logging.FileHandler(filename=outputfilename))
+    
+    # Python 3.9 introduced an encoding argument
+    if version_info[:2] >= (3,9):
+        kwargs = {'encoding': 'utf-8'}
+    else:
+        kwargs = {}
     # This is the only logger we'd ever use. However during testing, due to the context, logger can't be recreated,
     # even if it has already been shutdown. This is why we use force=True to force recreation of logger so we can
     # properly run our tests. Not the best solution, not sure if it's not prone to race conditions, but it is
     # at the very least safe to use for the actual software running
-    logging.basicConfig(handlers=handlers, encoding='utf-8', level=get_log_level(loglevel), format=loggingformat, datefmt='%Y-%m-%d %H:%M:%S', force=True)
+    logging.basicConfig(handlers=handlers, level=get_log_level(loglevel), format=loggingformat, datefmt='%Y-%m-%d %H:%M:%S', force=True, **kwargs)
