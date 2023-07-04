5bf35a7b0f18cfa6cf823ae2854a0e3b8bbf359c
XapaJIaMnu <nheart@gmail.com>
2023-07-04 02:05:27 +0100

    Fix existing tests

--- tests/test_endtoend.py
+++ tests/test_endtoend.py
@@ -6,28 +6,32 @@ import subprocess
 class TestEndToEnd(unittest.TestCase):
 	'''Tests the pipeline end-to-end. Aimed to to test the parser.'''
 	def test_full_enzh(self):
-		output: str = subprocess.check_output([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_config.yml', '-d', '--sync'], encoding="utf-8")
+		process = subprocess.run([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_config.yml', '-d', '--sync'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+		output = process.stdout
 		reference: str = ""
 		with open('contrib/test-data/test_enzh_config.expected.out', 'r', encoding='utf-8') as reffile:
 			reference: str = "".join(reffile.readlines())
 		self.assertEqual(output, reference)
 
 	def test_full_zhen(self):
-		output: str = subprocess.check_output([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_zhen_config.yml', '-d', '--sync'], encoding="utf-8")
+		process = subprocess.run([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_zhen_config.yml', '-d', '--sync'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+		output = process.stdout
 		reference: str = ""
 		with open('contrib/test-data/test_zhen_config.expected.out', 'r', encoding='utf-8') as reffile:
 			reference: str = "".join(reffile.readlines())
 		self.assertEqual(output, reference)
 
 	def test_prefix_augment(self):
-		output: str = subprocess.check_output([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_zhen_prefix_config.yml', '-d', '--sync'], encoding="utf-8")
+		process = subprocess.run([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_zhen_prefix_config.yml', '-d', '--sync'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+		output = process.stdout
 		reference: str = ""
 		with open('contrib/test-data/test_zhen_config_prefix.expected.out', 'r', encoding='utf-8') as reffile:
 			reference: str = "".join(reffile.readlines())
 		self.assertEqual(output, reference)
 
 	def test_no_shuffle(self):
-		output: str = subprocess.check_output([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_config_plain.yml', '-d', '-n'], encoding="utf-8")
+		process = subprocess.run([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_config_plain.yml', '-d', '-n'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+		output = process.stdout
 		reference: str = ""
 		with open('contrib/test-data/clean.enzh.10', 'r', encoding='utf-8') as reffile:
 			reference: str = "".join(reffile.readlines())
@@ -41,14 +45,16 @@ class TestEndToEnd(unittest.TestCase):
 				self.assertEqual(output_arr[i], reference_arr[i])
 
 	def test_advanced_config(self):
-		output: str = subprocess.check_output([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_tags_advanced_config.yml', '-d', '-n'], encoding="utf-8")
+		process = subprocess.run([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_tags_advanced_config.yml', '-d', '-n'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+		output = process.stdout
 		reference: str = ""
 		with open('contrib/test-data/test_enzh_tags_advanced_config.expected.out', 'r', encoding='utf-8') as reffile:
 			reference: str = "".join(reffile.readlines())
 		self.assertEqual(output, reference)
 
 	def test_stage_config(self):
-		output: str = subprocess.check_output([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_tags_stage_config.yml', '-d', '-n'], encoding="utf-8")
+		process = subprocess.run([sys.executable, '-m', 'opustrainer', '-c', 'contrib/test_enzh_tags_stage_config.yml', '-d', '-n'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+		output = process.stdout
 		reference: str = ""
 		with open('contrib/test-data/test_enzh_tags_stage_config.expected.out', 'r', encoding='utf-8') as reffile:
 			reference: str = "".join(reffile.readlines())
