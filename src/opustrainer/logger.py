f994a74934cf6d99ef7ef527ee38120c3810aed8
XapaJIaMnu <nheart@gmail.com>
2023-07-04 02:18:13 +0100

    Make typing happy

--- src/opustrainer/logger.py
+++ src/opustrainer/logger.py
@@ -1,6 +1,7 @@
+from io import TextIOWrapper
 import logging
 from sys import stderr
-from typing import List
+from typing import List, TextIO
 from functools import lru_cache
 
 def getLogLevel(name: str) -> int:
@@ -26,7 +27,8 @@ def log_once(msg: str, loglevel: str = "INFO") -> None:
 def setup_logger(outputfilename: str | None = None, loglevel: str = "INFO") -> None:
     """Sets up the logger with the necessary settings. Outputs to both file and stderr"""
     loggingformat = '[%(asctime)s] [Trainer] [%(levelname)s] %(message)s'
-    handlers = [logging.StreamHandler(stream=stderr)]
+    handlers: List[logging.StreamHandler[TextIO] | logging.StreamHandler[TextIOWrapper]] = []
+    handlers.append(logging.StreamHandler(stream=stderr))
     if outputfilename is not None:
         handlers.append(logging.FileHandler(filename=outputfilename))
     logging.basicConfig(handlers=handlers, encoding='utf-8', level=getLogLevel(loglevel), format=loggingformat, datefmt='%Y-%m-%d %H:%M:%S')
