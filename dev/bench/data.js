7f95c8635a74b300f7febbc39c2688b6844018ff
github-action-benchmark <github@users.noreply.github.com>
2023-09-13 14:03:48 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6c93fb667726504cad1cf5ab1aba140b99110f9d

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1694613828226,
+  "repoUrl": "https://github.com/hplt-project/OpusTrainer",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jelmer@ikhoefgeen.nl",
+            "name": "Jelmer van der Linde",
+            "username": "jelmervdl"
+          },
+          "committer": {
+            "email": "jelmer@ikhoefgeen.nl",
+            "name": "Jelmer van der Linde",
+            "username": "jelmervdl"
+          },
+          "distinct": true,
+          "id": "6c93fb667726504cad1cf5ab1aba140b99110f9d",
+          "message": "How about just using gh-pages?",
+          "timestamp": "2023-09-13T16:01:29+02:00",
+          "tree_id": "724ad07ca82f27126d3a26a5012ee22c68e0cf66",
+          "url": "https://github.com/hplt-project/OpusTrainer/commit/6c93fb667726504cad1cf5ab1aba140b99110f9d"
+        },
+        "date": 1694613827329,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 2712894284,
+            "unit": "ns"
+          },
+          {
+            "name": "Tags",
+            "value": 41708112880,
+            "unit": "ns"
+          },
+          {
+            "name": "Tags with SPM",
+            "value": 63836934617,
+            "unit": "ns"
+          }
+        ]
+      }
+    ]
+  }
+}
