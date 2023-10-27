3a59bd3f0848e5a5ced3412d092d7a0731a616a1
github-action-benchmark <github@users.noreply.github.com>
2023-10-27 13:34:09 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 9133e1525c7ee37f53ea14ee6a180152bf7ea192

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698413648623,
+  "lastUpdate": 1698413649815,
   "repoUrl": "https://github.com/hplt-project/OpusTrainer",
   "entries": {
     "Benchmark": [
@@ -77,6 +77,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Tags with SPM",
             "value": 63836934617,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jelmer@ikhoefgeen.nl",
+            "name": "Jelmer van der Linde",
+            "username": "jelmervdl"
+          },
+          "committer": {
+            "email": "jelmer@ikhoefgeen.nl",
+            "name": "Jelmer van der Linde",
+            "username": "jelmervdl"
+          },
+          "distinct": true,
+          "id": "9133e1525c7ee37f53ea14ee6a180152bf7ea192",
+          "message": "Fix PyYAML not installing properly?",
+          "timestamp": "2023-10-27T14:29:20+01:00",
+          "tree_id": "7ffa43a368d18b5f8dd816f6ba0b5639e6f834d7",
+          "url": "https://github.com/hplt-project/OpusTrainer/commit/9133e1525c7ee37f53ea14ee6a180152bf7ea192"
+        },
+        "date": 1698413648127,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 3799827790,
+            "unit": "ns"
+          },
+          {
+            "name": "Tags",
+            "value": 63935253597,
+            "unit": "ns"
+          },
+          {
+            "name": "Tags with SPM",
+            "value": 94124578240,
             "unit": "ns"
           }
         ]
